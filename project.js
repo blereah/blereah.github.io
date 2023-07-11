07244e641f2d027d73077c874361004d3d7e066b
blereah <138821924+blereah@users.noreply.github.com>
2023-07-11 15:20:51 -0500

    Add files via upload

--- project.js
+++ project.js
@@ -4,13 +4,11 @@ let playButton
 
 let numbers
 
-let upArrows
+
 
 
 function preload(){
 image = loadImage(numbers.png)
-
-image = loadImage(upArrows.png)
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -27,6 +25,10 @@ background(50, 88, 168)
 textSize(40)
 text("Welcome to Number Guesser", 500, 50);
 
-textSize(25)
-text("Press play button to begin", 50, 150)
+textSize(20)
+text("Press  button to begin", 50, 150)
+
+image(numbers, mouseX, mouseY, 100, 30)
 }
+
+
