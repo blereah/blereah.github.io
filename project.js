31336dc116bc33a3b09cc506ad7e5393fd6741e6
blereah <138821924+blereah@users.noreply.github.com>
2023-07-12 17:35:52 -0500

    Add files via upload

--- project.js
+++ project.js
@@ -1,34 +1,80 @@
-let Canvas
+let Canvas;
+let playButton;
+let numbers;
+let guessInput;
+let message;
+let secretNumber;
+let maxTries = 5;
+let tries = 0;
 
-let playButton
-
-let numbers
-
-
-
-
-function preload(){
-image = loadImage(numbers.png)
+function preload() {
+  image = loadImage("numbers.png");
 }
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  playButton = createButton("Play")
-  playButton.position(50, 30)
-  imageMode(CENTER)
+  playButton = createButton("Play");
+  playButton.position(50, 30);
+  playButton.mousePressed(startGame);
+  guessInput = createInput();
+  guessInput.position(50, 80);
+  guessInput.hide();
+  message = createElement("h2");
+  message.position(50, 120);
+  imageMode(CENTER);
 }
-
 
 function draw() {
+  background(50, 88, 168);
+  textSize(40);
+  text("Welcome to Number Guesser", 500, 50);
+  
+  textSize(20);
+  text("Press button to begin", 50, 150);
 
-background(50, 88, 168)
-textSize(40)
-text("Welcome to Number Guesser", 500, 50);
-
-textSize(20)
-text("Press  button to begin", 50, 150)
-
-image(numbers, mouseX, mouseY, 100, 30)
+  image(numbers, mouseX, mouseY, 100, 30);
 }
+
+function startGame() {
+    secretNumber = Math.floor(Math.random() * (100 - 1 +1)) +1;
+    tries = maxTries;
+    playButton.hide();
+    guessInput.show();
+    message.html(`You have ${tries} tries left`);
+}
+
+function keyPressed() {
+    if (keyCode === ENTER) {
+        let guess = parseInt(guessInput.value());
+        tries--;
+        if (guess === secretNumber) {
+            message.html(`You won! The secret number was ${secretNumber}`);
+            endGame();
+        } else if (tries === 0) {
+            message.html(`You lost! The secret number was ${secretNumber}`);
+            endGame();
+        } else if (guess > secretNumber) {
+            message.html(`Your guess is too high. You have ${tries} tries left`);
+        } else if (guess < secretNumber) {
+            message.html(`Your guess is too low. You have ${tries} tries left`);
+        }
+    }
+}
+
+function endGame() {
+    playButton.show();
+    guessInput.hide();
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
